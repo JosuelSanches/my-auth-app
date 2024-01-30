8b302c240200ec68e2f8a62b6a4eec34e21d99ec
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2024-01-30 17:02:49 -0300

    NextAuth.js
    Aprendendo a usar NextAuth.js.

--- app/components/SignInButton.tsx
+++ app/components/SignInButton.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+"use client"
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 const SignInButton = () => {
   return (
@@ -6,7 +7,7 @@ const SignInButton = () => {
         <button
                 type="button"
                 className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-              >
+              onClick={()=> signIn("google")}>
                 Login
               </button>
               <img
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import SignInButton from "./components/SignInButton";
 import "./globals.css";
+import Provider from "./components/Provider"
 
 export default function RootLayout({
   children,
@@ -9,6 +10,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Provider>
         <main className="flex min-h-screen flex-col items-center justify-between p-20">
           <div className="flex w-full max-w-full items-center justify-center">
             <ul className="flex items-center justify-between gap-10 text-xl">
@@ -20,6 +22,7 @@ export default function RootLayout({
           </div>
           {children}
         </main>
+        </Provider>
       </body>
     </html>
   );
