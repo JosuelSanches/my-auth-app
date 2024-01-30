94f8aed6db878f7b5ac5727a88a360a93dee3028
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2024-01-30 16:15:05 -0300

    UsingNextAuth.js
    Apredendo a usar o NextAuth.js em tela de login usando o google cloud.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import SignInButton from "./components/SignInButton";
 import "./globals.css";
 
 export default function RootLayout({
@@ -14,17 +15,7 @@ export default function RootLayout({
               <li>Home</li>
               <li>Server </li>
               <li>Client </li>
-              <button
-                type="button"
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-              >
-                Login
-              </button>
-              <img
-                className="w-10 h-10 rounded-full"
-                src="/DeveloperDeck101.png"
-                alt="Rounded avatar"
-              ></img>
+              <SignInButton/>
             </ul>
           </div>
           {children}
