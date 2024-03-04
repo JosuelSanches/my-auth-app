70b0698157941bd9ed18d7202985be6eda7536dd
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2024-03-04 12:49:52 -0300

    Skeleton loading UI
    Esse componente mostra um esqueleto de pagina flutuante que serve para mostrar ao usuário que a página está trabalhando para que o conteúdo seja mostrado.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  await new Promise((resolve) => setTimeout(resolve, 4000))
   return (
     <h1 className="text-green-500">Home Page</h1>
   );
