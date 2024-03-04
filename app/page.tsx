8820051988c27ef87b42cf0235178e1c8fbe1719
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2024-03-04 13:18:41 -0300

    React Suspense Loading UI
    Mostrando pagina suspensa enquanto espera resposta do servidor.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import Produto from "./components/Produto"
+import Review from "./components/Review"
+
 export default async function Home() {
   await new Promise((resolve) => setTimeout(resolve, 2000))
   return (
     <h1 className="text-green-500">Home Page
     <h2>
       <Produto/>
+      <br/>
+      <Review/>
     </h2>
     </h1>
   );
