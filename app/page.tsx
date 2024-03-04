d5cae75996fc20c76c23cba59185e2eb8c15c590
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2024-03-04 13:06:27 -0300

    React Suspense Loading
    Mostrando pagina de produtos após o carregamento do esqueleto suspenso.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
-
+import Produto from "./components/Produto"
 export default async function Home() {
-  await new Promise((resolve) => setTimeout(resolve, 4000))
+  await new Promise((resolve) => setTimeout(resolve, 2000))
   return (
-    <h1 className="text-green-500">Home Page</h1>
+    <h1 className="text-green-500">Home Page
+    <h2>
+      <Produto/>
+    </h2>
+    </h1>
   );
 }
